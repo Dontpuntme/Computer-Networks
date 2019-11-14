24c6fa553e96aca69c812cbbd601506791cbf010
Connor McLaughlin <cjmclaughlin@wpi.edu>
2019-11-14 15:13:13 -0500

    more progress

--- Project2-Wireview/wireview.c
+++ Project2-Wireview/wireview.c
@@ -1,34 +1,31 @@
 #include "wireview.h"
 
 void packetHandler(unsigned char *userData, const struct pcap_pkthdr* pkthdr, const unsigned char* packet) {
-    uint32_t size_ip = 0;
+    int size_ip = 0;
     struct ip* ip;
 
-    /* TODO find ethernet header related info */
-    // check ether_ntoa and cast to ether_addr
+    /* find ethernet header info */
     const struct ether_header* ether = (struct ether_header *)(packet);
     char ether_src[ETH_ADDR_LEN];
     char ether_dst[ETH_ADDR_LEN];
     ether_ntoa_r((struct ether_addr *)(ether->ether_shost), ether_src);
     ether_ntoa_r((struct ether_addr *)(ether->ether_dhost), ether_dst);
-    printf("Ethernet src: %s\t Ethernet dst: %s\n", ether_src, ether_dst); /* TODO fix */
+    printf("Ethernet src: %s\t Ethernet dst: %s\n", ether_src, ether_dst); /* TODO fix only returning destination*/
 
     if (ntohs(ether->ether_type) == ETHERTYPE_ARP) { /* check if ARP */
-        /* TODO determine ARP/IP, find related fields for ARP */
+        /* if ARP, we just have to return ethernet addresses (no IP/UDP) */
         printf("ARP packet\n");
     }
     else if (ntohs(ether->ether_type) == ETHERTYPE_IP) { /* check if IP */
         /* getting src/dst IP addresses */
-        ip =(struct ip*)(packet + ETH_HEAD_LEN); 
+        ip = (struct ip*)(packet + ETH_HEAD_LEN); 
         if (ip->ip_v != 4) {
             printf("IPv6 detected, dont care\n");
         }
         else {
-            printf("IPv4 detected\n");
-            // UDP --> ip_p == 17
             char ip_src[INET_ADDRSTRLEN];
             char ip_dst[INET_ADDRSTRLEN];
-            size_ip = (uint32_t)(ip->ip_hl * 4);
+            size_ip = (int)(ip->ip_hl * 4);
             if (size_ip < 20 || size_ip > 24) {
                 perror("Invalid IP size");
                 exit(1);
@@ -40,11 +37,13 @@ void packetHandler(unsigned char *userData, const struct pcap_pkthdr* pkthdr, co
             printf("IP src: %s\t IP dst: %s\n", ip_src, ip_dst); /* TODO figure out where/how we want to store this */
         }
     }
+    
+    /* if UDP, get src/dst */
+    if (ip->ip_p == 17) { // UDP --> ip_p == 17
+        struct udphdr* udp = (struct udphdr *)(packet + ETH_HEAD_LEN + size_ip);
+        printf("UDP src port: %d\tUDP dst port: %d\n", ntohs(udp->uh_sport), ntohs(udp->uh_dport)); // TODO fix
 
-    /* TODO determine if carrying UDP, if so print related ports */
-    if (ip->ip_p == 17) {
-        struct udphdr* udphdr = (struct udphr *)(packet + ETH_HEAD_LEN + size_ip);
-        printf("UDP src port: %d\tUDP dst port: %d\n", udphdr->uh_sport, udphdr->uh_dport); // TODO fix
+        // udp->uh_len for added offset to check payload (maybe has time)
     }
 
     /* TODO find start time of packet capture as well as duration */
