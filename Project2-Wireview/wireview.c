dce6268aa296c9340e6c33b6606d002973a6e56c
Connor McLaughlin <cjmclaughlin@wpi.edu>
2019-11-14 12:16:51 -0500

    src/dst ip function, new includes

--- Project2-Wireview/wireview.c
+++ Project2-Wireview/wireview.c
@@ -1,11 +1,26 @@
 #include "wireview.h"
 
 void packetHandler(unsigned char *userData, const struct pcap_pkthdr* pkthdr, const unsigned char* packet) {
-    printf("Hello packet\n");
+    /* getting src/dst IP addresses */
+    struct ip* ip =(struct ip*)(packet + ETH_HEAD_LEN); 
+    char ip_src[INET_ADDRSTRLEN];
+    char ip_dst[INET_ADDRSTRLEN];
+    uint32_t size_ip = (uint32_t)(ip->ip_hl * 4);
+    if (size_ip < 20 || size_ip > 24) {
+        perror("Invalid IP size");
+        exit(1);
+    }
+    else {
+        inet_ntop(AF_INET, &(ip->ip_src), ip_src, INET_ADDRSTRLEN);
+        inet_ntop(AF_INET, &(ip->ip_dst), ip_dst, INET_ADDRSTRLEN);
+    }
+    printf("IP src:%s\t IP dst:%s\n", ip_src, ip_dst);
+
+
+    packetInfo.totalPackets++;
 }
 
 int main(int argc, char** argv) {
-	char *dev;
     char errbuf[PCAP_ERRBUF_SIZE];
     char* filename;
     pcap_t* desc;
@@ -28,4 +43,6 @@ int main(int argc, char** argv) {
     //meants process packets until another conditions comes up packetHandler is run on each packet
     pcap_loop(desc,-1,packetHandler,NULL);
     pcap_close(desc);
+
+    printf("Total packets: %d\n", packetInfo.totalPackets);
 }
--- Project2-Wireview/wireview.h
+++ Project2-Wireview/wireview.h
@@ -1,11 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <pcap/pcap.h>
 #include <net/ethernet.h>
+#include <netinet/ether.h>
 #include <netinet/udp.h>
 #include <netinet/ip.h>
 #include <arpa/inet.h>
 
 #define ETH_ADDR_LEN 6
 #define ETH_HEAD_LEN 14
+
+struct packetInfo {
+    int totalPackets;
+
+};
+
+/* from tcpdump.org/pcap.html */
+/* IP header */
+struct sniff_ip {
+    unsigned char ip_vhl;		/* version << 4 | header length >> 2 */
+	unsigned char ip_tos;		/* type of service */
+	unsigned char ip_len;		/* total length */
+	unsigned char ip_id;		/* identification */
+	unsigned char ip_off;		/* fragment offset field */
+	unsigned char ip_ttl;		/* time to live */
+	unsigned char ip_p;		/* protocol */
+	unsigned char ip_sum;		/* checksum */
+	struct in_addr ip_src,ip_dst; /* source and dest address */
+};
+#define IP_HL(ip)		(((ip)->ip_vhl) & 0x0f)
+
+
+
+struct packetInfo packetInfo; /* global struct for accumulating data */
 
 void packetHandler(unsigned char *userData, const struct pcap_pkthdr* pkthdr, const unsigned char* packet);
 
