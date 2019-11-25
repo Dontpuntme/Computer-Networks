596f72ad8b0d3df52f5a2d6b8f071f3e669267e5
Connor McLaughlin <cjmclaughlin@wpi.edu>
2019-11-25 16:59:37 -0500

    Co-authored-by: Dontpuntme <david.martindale.99@gmail.com>

--- Project3-OverlayNetwork/overlay.cpp
+++ Project3-OverlayNetwork/overlay.cpp
@@ -1,25 +1,78 @@
 #include "overlay.h"
-#include <string.h>
+#include <getopt.h>
+#include "getopt.h"
+#include <iostream>
+#include <fstream>
+#include "string.h"
 
 void printUsage() {
-    printf("Usage: ./overlay <mode> ...\n where mode in [router, end-host]");
+    std::cout << "Router Usage: ./overlay --router <ip mappings>" << std::endl;
+    std::cout << "End-Host Usage: ./overlay --host <router ip> <host ip> <ttl>" << std::endl;
 }
 
-int main(int argc, char** args) {
+int main(int argc, char** argv) {
+    bool router = false;
+    bool host = false;
+    char* ipMappings;
+    char* routerIP;
+    char* hostIP;
+    uint32_t ttl;
+
     if (argc <= 1) {
         printUsage();
         exit(1);
     }
     if (argc > 1) {
-        if (strcmp(args[1], "router") == 0) {
-            printf("Running in router mode\n");
-        }
-        else if (strcmp(args[1], "end-host") == 0) {
-            printf("Running as end-host\n");
-        }
-        else {
-            printf("Argument \"%s\" does not match one of [router, end-host]\n", args[1]);
+        const char* const short_opts = "r:h:";
+        const option long_opts[] = {
+            {"router", required_argument, nullptr, 'r'},
+            {"host", required_argument, nullptr, 'h'}
+        };
+        while(true) {
+            const auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
+            if(-1 == opt) break;
+            switch (opt) {
+            case 'h':
+                if (argc < 4) {
+                    printUsage();
+                    return 1;
+                }
+                else {
+                    routerIP = argv[2];
+                    hostIP = argv[3];
+                    ttl = atoi(argv[4]);
+                    printf("Running in host mode with routerIP:%s | hostIP:%s | ttl:%d\n", routerIP, hostIP, ttl);
+                    host = true;
+                    runEndHost(routerIP, hostIP, ttl);
+                    break;
+                }
+            case 'r':
+                if(argc < 2){
+                    printUsage();
+                    return 1;
+                }
+                else {
+                    ipMappings = argv[2];
+                    printf("Running in router mode with IP mappings: %s\n", ipMappings);
+                    router = true;
+                    runRouter(ipMappings);
+                    break;
+                }                
+            default:
+                printUsage();
+                break;
+            }
         }
     }
     return 0;
+}
+
+/* run process as router */
+int runRouter(char* ipMappings) {
+
+}
+
+/* run process as end-host */
+int runEndHost(char* routerIP, char* hostIP, uint32_t ttl) {
+
 }
