57032b2cf7615e177bde55539dc662ff1f83a807
Connor McLaughlin <cjmclaughlin@wpi.edu>
2019-12-05 14:13:14 -0500

    gaa

--- Project3-OverlayNetwork/overlay.cpp
+++ Project3-OverlayNetwork/overlay.cpp
@@ -116,13 +116,15 @@ int routePacket(char *packet, std::vector<std::string> &overlayIPs, std::vector<
     uint32_t max = overlayIPs.size();
     bool foundMatch = false;
     for (uint32_t i = 0; i < max; i++) {
-        char tableIP[INET_ADDRSTRLEN] = overlayIPs.at[i];
+        const char* tableIP = (char*)malloc(sizeof(char)*INET_ADDRSTRLEN);
+        tableIP = overlayIPs.at(i).c_str();
         printf("Checking overlay IP: %s against IP in table: %s\n", strOverlayIP, tableIP);
         if ((strcmp(strOverlayIP, overlayIPs[i].c_str()) == 0)) {
             printf("Found match for overlay IP in routing table!\n");
             foundMatch = true;
             break;
         }
+        free((void*)tableIP);
         printf("Overlay IP: %s\n", overlayIPs[i].c_str());
     }
     if (!foundMatch) {
